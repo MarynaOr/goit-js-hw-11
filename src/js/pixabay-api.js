a058ddbc1370a5a92b93722a6592c13a27e47869
Maryna <89296066105marina@gmal.com>
2024-12-15 14:46:35 +0100

    pix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,48 @@
+export default fetchImg
+
+
+const API_KEY = ' 47645708-0e2c2a3797788683efa376261'
+const URL = 'https://pixabay.com/api'
+
+function fetchImg (query) {
+
+    return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
+.then((response) => {
+    if(!response.ok){
+        iziToast.error({
+            title: 'Error',
+            message: 'Sorry, there are no images matching your search query. Please try again!',
+        });
+
+
+    }
+    return response.json();
+
+})
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/main.js
@@ -0,0 +1,46 @@
+import fetchImg from "./js/pixabay-api";
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const form =document.querySelector('.form');
+const btn =  document.querySelector('.btn');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
